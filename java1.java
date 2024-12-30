32fa755926357b38db40282bc3d6c152fc0aef32
Naven7009 <navenrajselvam@gmail.com>
2024-12-30 13:02:09 +0530

    new file

---
+++ java1.java
@@ -0,0 +1,13 @@
+public class java1{
+    int a =101;
+    String b ="jaaaava__1";
+
+    java1(int a, String b){
+        this.b =b;
+        System.out.println("constructor" +this.a + this.b);
+    }
+    public void method1(){
+        System.out.println("java method");
+    }
+}
+
